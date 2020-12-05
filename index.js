93f6c6b7f239be0b14b298cd0470f441a8b39dc3
aldrinburgos <burgos.aldrin@gmail.com>
2020-12-05 01:25:06 -0600

    added generate html function

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const inquirer = require('inquirer');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
+const generateHTML = require('./lib/generateHTML');
+
 const employees = [];
+
 
 function addManager() {
     console.log("Hello! Please follow instructions to generate HTML!")
@@ -11,22 +14,56 @@ function addManager() {
         [{
             type: 'text',
             name: 'managerName',
-            message: "Enter team manager's name: "
+            message: "Enter team manager's name: ",
+            validate: managerName => {
+                const val = /^[a-zA-Z]+$/;
+                if (managerName.match(val)) {
+                    return true;
+                } else {
+                    console.log('\n Please enter a valid name.');
+                    return false;
+                }
+            }
         },
         {
             type: 'text',
             name: 'managerEmail',
-            message: "Enter team manager's e-mail: "
+            message: "Enter team manager's e-mail: ",
+            validate: managerEmail => {
+                const val = /([a-zA-Z0-9._])+@([a-zA-Z])+(\.[a-zA-Z]+)+/;
+                if (managerEmail.match(val)) {
+                    return true;
+                } else {
+                    console.log('\n Please enter a valid e-mail address.');
+                    return false;
+                }
+            }
         },
         {
             type: 'text',
             name: 'managerId',
-            message: "Enter team manager's ID number: "
+            message: "Enter team manager's ID number: ",
+            validate: managerId => {
+                if (isNaN(managerId)){
+                    console.log('\n Please input numbers only.')
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         },
         {
             type: 'text',
             name: 'officeNumber',
-            message: "Enter team manager's office number: "
+            message: "Enter team manager's office number: ",
+            validate: officeNumber => {
+                if (isNaN(officeNumber)) {
+                    console.log('\n Please input numbers only.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         }]
         ).then((data) => {
             let manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.officeNumber, data.role);
@@ -40,17 +77,42 @@ function addEngineer(pickRole) {
         .prompt([{
             type: 'text',
             name: 'name',
-            message: "Enter engineer's name: "
+            message: "Enter engineer's name: ",
+            validate: name => {
+                const val = /^[a-zA-Z]+$/;
+                if (name.match(val)) {
+                    return true;
+                } else {
+                    console.log('\n Please enter a valid name.');
+                    return false;
+                }
+            }
         },
         {
             type: 'text',
             name: 'email',
-            message: "Enter engineer's e-mail: "
+            message: "Enter engineer's e-mail: ",
+            validate: email => {
+                const val = /([a-zA-Z0-9._])+@([a-zA-Z])+(\.[a-zA-Z]+)+/;
+                if (email.match(val)) {
+                    return true;
+                } else {
+                    console.log('\n Please enter a valid e-mail address.')
+                }
+            }
         },
         {
             type: 'text',
             name: 'id',
-            message: "Enter engineer's ID number: "
+            message: "Enter engineer's ID number: ",
+            validate: id => {
+                if (isNaN(id)) {
+                    console.log('Please input numbers only');
+                    return false
+                } else {
+                    return true;
+                }
+            }
         },
         {
             type: 'text',
@@ -68,7 +130,6 @@ function addEngineer(pickRole) {
             let engineer = new Engineer(data.name, data.id, data.email, data.github, pickRole.role);
             employees.push(engineer);
             addEmployee();
-            console.log(employees);
     })
 };
 
@@ -77,17 +138,43 @@ function addIntern(pickRole) {
         .prompt([{
             type: 'text',
             name: 'name',
-            message: "Enter intern's name: "
+            message: "Enter intern's name: ",
+            validate: name => {
+                const val = /^[a-zA-Z]+$/;
+                if (name.match(val)) {
+                    return true;
+                } else {
+                    console.log('\n Please enter a valid name.');
+                    return false;
+                }
+            }
         },
         {
             type: 'text',
             name: 'email',
-            message: "Enter intern's e-mail: "
+            message: "Enter intern's e-mail: ",
+            validate: email => {
+                const val = /([a-zA-Z0-9._])+@([a-zA-Z])+(\.[a-zA-Z]+)+/;
+                if (email.match(val)) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid e-mail address.');
+                    return false;
+                }
+            }
         },
         {
             type: 'text',
             name: 'id',
-            message: "Enter intern's ID number: "
+            message: "Enter intern's ID number: ",
+            validate: id => {
+                if (isNaN(id)) {
+                    console.log('Please input numbers only');
+                    return false
+                } else {
+                    return true;
+                }
+            }
         },
         {
             type: 'text',
@@ -105,7 +192,6 @@ function addIntern(pickRole) {
             let intern = new Intern(data.name, data.id, data.email, data.school, pickRole.role);
             employees.push(intern);
             addEmployee();
-            console.log(employees);
     })
 };
 
@@ -122,9 +208,10 @@ function addEmployee() {
         } else if (pickRole.role === 'Intern') {
             addIntern(pickRole);
         } else {
-         //
+            generateHTML(employees);
         }
     })
 };
+
 
 addManager();
